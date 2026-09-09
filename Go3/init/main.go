cdbe2168372a2d8863205b54b4c1c90fc4b1cd98
Frostmourneee <wellthx1@gmail.com>
2026-09-09 23:33:26 +0300

    init3

---
+++ Go3/init/main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"context"
+	// "fmt"
+	"sync"
+)
+
+func main() {
+	inputs := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	fn := func(i int) int {
+		return i * 2
+	}
+	workers := 5
+	results, err := ParallelMapCtx(context.Background(), inputs, fn, workers)
+	if err != nil {
+		// log.Fatalf("ParallelMapCtx: %v", err)
+	}
+	// fmt.Println(results)
+}
+
+func ParallelMapCtx(ctx context.Context, inputs []int, fn func(int) int, workers int) ([]int, error) {
+	n := len(inputs)
+	wg := &sync.WaitGroup{}
+	wg.Add(2 * n)
+
+	results := make([]int, n)
+	workersChannel := make(chan bool, workers)
+	jobResult := make(chan struct {
+		index int
+		num   int
+	}, n)
+
+	ctxWithCancel, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	go func(ctx context.Context) {
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case result := <-jobResult:
+				wg.Done()
+				results[result.index] = result.num
+			}
+		}
+	}(ctxWithCancel)
+
+	for i := 0; i < workers; i++ {
+		if err := ctx.Err(); err != nil {
+			return nil, err
+		}
+
+		go func(i int) {
+			defer wg.Done()
+			
+			jobResult <- struct {
+				index int
+				num   int
+			}{i, fn(inputs[i])}
+			workersChannel <- true
+		}(i)
+	}
+
+	for i := workers; i < n; i++ {
+		select {
+		case <-ctx.Done():
+			return nil, ctx.Err()
+		case <-workersChannel:
+			go func(i int) {
+				defer wg.Done()
+
+				jobResult <- struct {
+					index int
+					num   int
+				}{i, fn(inputs[i])}
+				workersChannel <- true
+			}(i)
+		}
+	}
+
+	wg.Wait()
+
+	return results, nil
+}
